50e2fdc225ae4cf964de604e092346e3edba6dc0
sxend <arimitsu.k+github@gmail.com>
2017-06-12 22:36:15 +0900

    add ws path

--- src/main/scala/arimitsu/sf/benchmark/Bootstrap.scala
+++ src/main/scala/arimitsu/sf/benchmark/Bootstrap.scala
@@ -1,27 +1,42 @@
 package arimitsu.sf.benchmark
 
+import java.util.UUID
+
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.Directives._
-import spray.json.DefaultJsonProtocol._
+import spray.json._
+import DefaultJsonProtocol._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.model.ws.{ Message, TextMessage }
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{ Flow, Tcp }
-import akka.util.ByteString
-import org.apache.commons.lang3.RandomStringUtils
+import akka.stream.scaladsl.Flow
+
+import scala.concurrent.Future
 
 object Bootstrap {
   implicit val format = jsonFormat1(JsonResponse)
-  def main(args: Array[String]): Unit = {
-    implicit val system = ActorSystem("benchmark-system")
-    implicit val materializer = ActorMaterializer()
+  private def route = {
     val jsonRoute = get(path("json")(complete(JsonResponse(payload))))
     val plainTextRoute = get(path("plaintext")(complete(payload)))
-    val route = jsonRoute ~ plainTextRoute
+    val wsRoute = path("ws") {
+      extractUpgradeToWebSocket { upgrade =>
+        complete(upgrade.handleMessages {
+          Flow[Message].mapAsync(16) { _ =>
+            Future.successful(TextMessage(JsonResponse(payload).toJson.compactPrint))
+          }
+        })
+      }
+    }
+    jsonRoute ~ plainTextRoute ~ wsRoute
+  }
+  def main(args: Array[String]): Unit = {
+    implicit val system = ActorSystem("benchmark")
+    implicit val materializer = ActorMaterializer()
     val routeHandler = Route.asyncHandler(route)
     Http().bindAndHandleAsync(routeHandler, "0.0.0.0", 9000, parallelism = 16)
   }
-  private def payload = RandomStringUtils.randomAlphanumeric(100)
+  private def payload = UUID.randomUUID().toString
   case class JsonResponse(message: String)
 }
